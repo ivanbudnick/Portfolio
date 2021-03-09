7656d4a2c051692b3586cb318a2ef270aa999327
ivanbudnick <ivan.budnick@gmail.com>
2021-03-08 23:34:25 -0300

    Prueba animaciones

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hola")
+let bottomHeight = window.scrollY;
+
+function debounce(func, wait = 20, immediate = true) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
+amed = document.getElementById("desc-amed")
+ n = 0;
+function showHeight(){
+    n++;
+    console.log(n);
+    console.log(bottomHeight);
+}
+
+window.addEventListener("scroll", debounce(showHeight));
